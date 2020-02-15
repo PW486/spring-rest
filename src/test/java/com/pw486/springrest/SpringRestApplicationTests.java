ce5e3e582d944d4e91d37d7c7f1903cf999e19bd
PW486 <ooqwe486@gmail.com>
2020-02-15 14:29:59 +0900

    Initial commit

---
+++ src/test/java/com/pw486/springrest/SpringRestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pw486.springrest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRestApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
