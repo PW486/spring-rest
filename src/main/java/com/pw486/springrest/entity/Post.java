b7187d0579d84967f964a7e0347e03c2dd5c5e7a
PW486 <ooqwe486@gmail.com>
2020-02-15 15:17:46 +0900

    Create Post entity

---
+++ src/main/java/com/pw486/springrest/entity/Post.java
@@ -0,0 +1,72 @@
+package com.pw486.springrest.entity;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import java.util.Date;
+
+@Entity
+@Table(name = "posts")
+public class Post {
+  @Id()
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  public Long id;
+
+  @NotBlank
+  public String title;
+
+  @NotBlank
+  public String text;
+
+  @Column(nullable = false, updatable = false)
+  @Temporal(TemporalType.TIMESTAMP)
+  @CreatedDate
+  public Date createdAt;
+
+  @Column(nullable = false)
+  @Temporal(TemporalType.TIMESTAMP)
+  @LastModifiedDate
+  public Date updatedAt;
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getTitle() {
+    return title;
+  }
+
+  public void setTitle(String title) {
+    this.title = title;
+  }
+
+  public String getText() {
+    return text;
+  }
+
+  public void setText(String text) {
+    this.text = text;
+  }
+
+  public Date getCreatedAt() {
+    return createdAt;
+  }
+
+  public void setCreatedAt(Date createdAt) {
+    this.createdAt = createdAt;
+  }
+
+  public Date getUpdatedAt() {
+    return updatedAt;
+  }
+
+  public void setUpdatedAt(Date updatedAt) {
+    this.updatedAt = updatedAt;
+  }
+}
